178238b8fb1c5f0bd66c80049bf0f7f41bfe0988
rm-dantz <dantz@rmgames.net>
2015-01-28 15:51:22 +0900

    몬스터 / 스테이지 파싱 틀 만듬
    적용은 추후에..

---
+++ workspace/Assets/Scripts/Game/data/obj/stageData.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stageData : MonoBehaviour {
+
+	public stageData(){
+
+	}
+
+	public int 			stageID;
+	public int 			type;
+	public string[] 	itemLevel;
+	public int			length;
+	public string[] 	obstacleRate;
+	public int 			obstacleID;
+	public string[] 	treasureRate;
+	public string[] 	wandRate;
+	public string[] 	monsterRate;
+	public int 			monsterMax;
+	public string[] 	monsterLevel;
+	public string[] 	monsterPool;
+	public int 			resetWandType;
+
+	public stageData(stageData _stage){
+		stageID 		=_stage.stageID;
+		type 			=_stage.type;
+		itemLevel 		=_stage.itemLevel;
+		length 			=_stage.length;
+		obstacleRate 	=_stage.obstacleRate;
+		obstacleID 		=_stage.obstacleID;
+		treasureRate 	=_stage.treasureRate;
+		wandRate 		=_stage.wandRate;
+		monsterRate 	=_stage.monsterRate;
+		monsterMax 		=_stage.monsterMax;
+		monsterLevel 	=_stage.monsterLevel;
+		monsterPool 	=_stage.monsterPool;
+		resetWandType 	=_stage.resetWandType;
+	}
+
+
+	~stageData(){
+
+	}
+}
